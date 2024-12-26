03187174184294f88fd521a5e68cdd9a1274083f
Paigro <paigro9@gmail.com>
2024-12-26 17:27:36 +0100

    Cosas

--- Libros/Libros/DataLibro.cpp
+++ Libros/Libros/DataLibro.cpp
@@ -9,3 +9,15 @@ DataLibro::~DataLibro()
 {
 
 }
+
+void DataLibro::setStartDate(int d, int m, int y)
+{
+}
+
+void DataLibro::setEndDate(int d, int m, int y)
+{
+}
+
+void DataLibro::setBook(std::string t, std::string a)
+{
+}
